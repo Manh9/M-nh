28f5fb9b767d4fed307ae617514e2bc2fafebd88
manh <manh199666@gmail.com>
2022-08-26 11:19:12 +0700

    26-8

---
+++ main1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	printf("chao moi nguoi .\n");
+	scanf("%d" , &a);
+	printf("chao moi nguoi . %d\n" , a);
+	return 0;
+}
