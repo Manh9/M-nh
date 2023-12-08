2718eeef0dcfd4ece238f2b23d99ff4a869af0d1
manh <manh199666@gmail.com>
2023-12-08 20:55:47 +0700

    Bai_thi_ASPcore

---
+++ Bai_thi_ASPcore/Bai_thi_ASPcore/Models/EmployeeDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bai_thi_ASPcore.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext() { }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+          : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-RNHMD3P\\SQLEXPRESS;Initial Catalog=APSCoreMVC;Persist Security Info=True;User ID=sa;Password=531999;Encrypt=False");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
